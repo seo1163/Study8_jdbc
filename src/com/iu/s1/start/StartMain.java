490958b870d3df21edb55fef473791807b3296ed
seo1163 <seo1163@naver.com>
2022-01-19 12:36:40 +0900

    add2_main

---
+++ src/com/iu/s1/start/StartMain.java
@@ -0,0 +1,13 @@
+package com.iu.s1.start;
+
+public class StartMain {
+
+	public static void main(String[] args) {
+		System.out.println("DB 연동 테스트 시작");
+		
+		
+		System.out.println("DB 연동 테스트 끝");
+
+	}
+
+}
